f221bbcad91803052a78dc8eca2113e8f639ed12
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-07-15 21:51:21 +0100

    easier config map

--- src/yoyo/clj_nats_async.clj
+++ src/yoyo/clj_nats_async.clj
@@ -2,9 +2,10 @@
   (:require [clj-nats-async.core :as nats]
             [clojure.tools.logging :as log]))
 
-(defn with-nats-client [{:keys [server-urls]} f]
-  (log/infof "Starting NATS client against servers: %s" server-urls)
-  (let [client (nats/create-nats server-urls)]
+(defn with-nats-client [{:keys [nats-opts]} f]
+  (log/infof "Starting NATS client with opts: %s" (prn-str nats-opts))
+  (let [{:keys [server-urls]} nats-opts
+        client (nats/create-nats server-urls)]
     (log/info "Started NATS client.")
     (try
       (f client)
