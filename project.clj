49e2221df109d7608bcc6e5c527c27e96adda044
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-07-23 18:04:05 +0100

    update to 0.4.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject yoyo-clj-nats-async "0.3.0"
+(defproject yoyo-clj-nats-async "0.4.0"
   :description "yoyo constructor for clj-nats-async clients"
   :url "https://github.com/employeerepublic/yoyo-clj-nats-async"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[clj-nats-async "0.3.0"]
+  :dependencies [[clj-nats-async "0.4.0"]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.7.0"]]}})
